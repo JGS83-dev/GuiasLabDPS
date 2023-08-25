ee4b5d9da1dd30ae43f1de384b8c73939a9afcb5
jgs23 <js8215001@gmail.com>
2023-08-25 08:37:55 -0600

    Agregado modal para detalle de producto

--- GuiasLabDPS/cart-react/src/components/ProductList.js
+++ GuiasLabDPS/cart-react/src/components/ProductList.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { data } from "../data";
+import Modal from "react-modal";
+
 export const ProductList = ({
   allProducts,
   setAllProducts,
@@ -8,6 +10,16 @@ export const ProductList = ({
   total,
   setTotal,
 }) => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [selectProduct, setSelectProduct] = React.useState({
+    id: 1,
+    title: "Cien años de soledad",
+    price: 100,
+    urlImage: "https://images.penguinrandomhouse.com/cover/9780525562443",
+    quantity: 1,
+    descripcion: "",
+  });
+
   const onAddProduct = (product) => {
     if (allProducts.find((item) => item.id === product.id)) {
       const products = allProducts.map((item) =>
@@ -21,12 +33,26 @@ export const ProductList = ({
     setCountProducts(countProducts + product.quantity);
     setAllProducts([...allProducts, product]);
   };
+
+  const showBookInfo = (product) => {
+    setSelectProduct(product);
+    setIsOpen(true);
+  };
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="container-items">
       {data.map((product) => (
         <div className="item" key={product.id}>
           <figure>
-            <img src={product.urlImage} alt={product.title} />
+            <img
+              src={product.urlImage}
+              alt={product.title}
+              onClick={() => showBookInfo(product)}
+            />
           </figure>
           <div className="info-product">
             <h2>{product.nameProduct}</h2>
@@ -35,6 +61,23 @@ export const ProductList = ({
               Añadir al carrito
             </button>
           </div>
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Detalle libro"
+          >
+            <div className="item" key={selectProduct.id}>
+              <figure>
+                <img src={selectProduct.urlImage} alt={selectProduct.title} />
+              </figure>
+              <div className="info-product">
+                <h2>{selectProduct.nameProduct}</h2>
+                <p className="price">${selectProduct.price}</p>
+                <p className="price">${selectProduct.descripcion}</p>
+                <button onClick={closeModal}>Cerrar</button>
+              </div>
+            </div>
+          </Modal>
         </div>
       ))}
     </div>
