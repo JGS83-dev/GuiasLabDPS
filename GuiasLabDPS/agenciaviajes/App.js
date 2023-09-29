ce15def989a59f37351925a7c6e196eb4f426f61
jgs23 <js8215001@gmail.com>
2023-09-29 07:29:02 -0600

    Configuracion inicial

--- GuiasLabDPS/agenciaviajes/App.js
+++ GuiasLabDPS/agenciaviajes/App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.contenedor}>
+        <View style={styles.caja1}></View>
+        <View style={styles.caja2}></View>
+        <View style={styles.caja3}></View>
+        <View style={styles.caja4}></View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contenedor: {
+    backgroundColor: "cornflowerblue",
+    flex: 1,
+    flexDirection: "column",
+  },
+  caja1: {
+    padding: 20,
+    backgroundColor: "navy",
+  },
+  caja2: {
+    padding: 20,
+    backgroundColor: "yellow",
+  },
+  caja3: {
+    padding: 20,
+    backgroundColor: "green",
+  },
+  caja4: {
+    padding: 20,
+    backgroundColor: "teal",
   },
 });
