bd4e62651898155ecdf2166ad5ae460cea020056
jgs23 <js8215001@gmail.com>
2023-10-13 08:21:06 -0600

    Ajustes a App.js

--- GuiasLabDPS/paises/App.js
+++ GuiasLabDPS/paises/App.js
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Formulario from "./src/componentes/Formulario";
 
 export default function App() {
   const [busqueda, setBusqueda] = useState({ pais: "" });
+  const [consultar, setconsultar] = useState(false);
+  const [resultado, setresultado] = useState({});
+
+  useEffect(() => {
+    const { pais } = busqueda;
+    const consultarPais = async () => {
+      if (consultar) {
+        const url = `https://servicodados.ibge.gov.br/api/docs/paises/${pais}`;
+        try {
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+          setresultado(resultado);
+          setconsultar(false);
+        } catch (error) {
+          mostrarAlerta();
+        }
+      }
+    };
+    consultarPais();
+  }, [consultar]);
 
   return (
     <View style={styles.app}>
       <StatusBar style="auto" />
       <View style={styles.contenido}>
-        <Formulario busqueda={busqueda} guardarBusqueda={setBusqueda} />
+        <Formulario
+          busqueda={busqueda}
+          guardarBusqueda={setBusqueda}
+          guardarConsulta={setconsultar}
+        />
       </View>
     </View>
   );
