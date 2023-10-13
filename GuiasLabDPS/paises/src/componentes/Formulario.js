2037bc07d8266ea389777ea10304302fe6186c85
jgs23 <js8215001@gmail.com>
2023-10-13 08:03:13 -0600

    Ajustes a componente Formulario

--- GuiasLabDPS/paises/src/componentes/Formulario.js
+++ GuiasLabDPS/paises/src/componentes/Formulario.js
@@ -1,25 +1,69 @@
 import { Picker } from "@react-native-picker/picker";
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableNativeFeedback, View } from "react-native";
+import {
+  Alert,
+  Animated,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  View,
+} from "react-native";
 import { paises } from "../data/paises";
 
-const Formulario = ({ busqueda, guardarBusqueda }) => {
+const Formulario = ({ busqueda, guardarBusqueda, guardarConsulta }) => {
   const { pais } = busqueda;
+
+  const consultarPais = () => {
+    if (pais.trim() === "") {
+      mostrarAlerta();
+      return;
+    }
+
+    guardarConsulta(true);
+  };
+
+  const mostrarAlerta = () => {
+    Alert.alert("Error", "Debe seleccionar un pais", [{ Text: "Entendido" }]);
+  };
+
+  const animacionSalida = () => {
+    Animated.spring(animacionBoton, {
+      toValue: 1,
+      useNativeDriver: true,
+      duration: 500,
+    }).start();
+  };
+
+  const estiloAnimacion = () => {
+    transform: [{ scale: animacionBoton }];
+  };
+
   return (
     <>
       <View>
         <View>
           <Text style={styles.input}>Pais</Text>
         </View>
-        <Picker style={styles.itemPais} selectedValue={pais}>
+        <Picker
+          style={styles.itemPais}
+          selectedValue={pais}
+          onValueChange={(pais) => guardarBusqueda({ ...busqueda, pais })}
+          onPress={() => consultarPais()}
+        >
           {paises.map((ele) => (
             <Picker.Item label={ele.label} value={ele.value} />
           ))}
         </Picker>
-        <TouchableNativeFeedback>
-          <View style={styles.btnBuscar}>
-            <Text style={styles.textoBuscar}>Buscar País</Text>
-          </View>
+        <TouchableNativeFeedback
+          onPressIn={() => animacionEntrada()}
+          onPressOut={() => animacionSalida()}
+          onPress={() => consultarPais()}
+        >
+          <Animated.View style={[styles.btnBuscar, estiloAnimacion]}>
+            <View style={styles.btnBuscar}>
+              <Text style={styles.textoBuscar}>Buscar País</Text>
+            </View>
+          </Animated.View>
         </TouchableNativeFeedback>
       </View>
     </>
