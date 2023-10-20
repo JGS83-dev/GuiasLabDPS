20e80b24197574f27db65806b754e25671d3e98e
jgs23 <js8215001@gmail.com>
2023-10-20 07:23:13 -0600

    Nuevas pantallas y librerias

---
+++ GuiasLabDPS/navegacion/src/screens/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Contact() {
+  return (
+    <View>
+      <Text>Estamos en Contact</Text>
+    </View>
+  );
+}
+
+npm i --save @react-navigation/native @react-navigation/stack @react-native-community/masked-view react-native-safe-area-context  react-native-screens react-native-gesture-handler
